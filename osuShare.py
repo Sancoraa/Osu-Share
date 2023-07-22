df2be8199a9ffc480f4ac3f2cc9ecb5c49231fc5
unclem2 <74730671+unclem2@users.noreply.github.com>
2023-07-22 03:22:28 +0300

    autodetect 'output.txt' and switch to requests

--- osuShare.py
+++ osuShare.py
@@ -1,42 +1,57 @@
 import os
 import re
-import subprocess
+import requests
 from datetime import datetime
 
-# Get and format date
+# Получаем путь к текущей директории скрипта
+current_directory = os.getcwd()
+
+# Получаем и форматируем текущую дату и время
 current_datetime = datetime.now()
 date_str = current_datetime.strftime("%Y-%m-%d_%H-%M-%S")
 
-choice = input('Do you want to import or export songs ? (imp/exp)\n')
+choice = input('Do you want to import or export songs? (imp/exp)\n')
 
 if choice == 'exp':
-	output_filename = f"output_{date_str}.txt" #Create file
-	songFolder = input('Enter the path of your osu! songs folder:\n')
-	pattern = re.compile(r'^(\d+)')
+    # Формируем имя файла вывода
+    output_filename = os.path.join(current_directory, f"output_{date_str}.txt")
+    songFolder = input('Enter the path of your osu! songs folder:\n')
+    pattern = re.compile(r'^(\d+)')
 
-	with open(output_filename, 'w') as file:
-		entries = os.listdir(songFolder)
-		print('The file ' + output_filename + ' has been created.')
-		for entry in entries:
-			match = pattern.match(entry)
-			if match:
-				number_part = match.group(1)
-				file.write(number_part + "\n")
+    with open(output_filename, 'w') as file:
+        entries = os.listdir(songFolder)
+        print('The file ' + output_filename + ' has been created.')
+        for entry in entries:
+            match = pattern.match(entry)
+            if match:
+                number_part = match.group(1)
+                file.write(number_part + "\n")
 
 elif choice == 'imp':
-	pathChoice = input('Enter the path of the txt file:\n')
-	current_directory = os.path.dirname(os.path.abspath(__file__))
-	directory_name = date_str
-	new_directory = os.path.join(current_directory, directory_name)
-	desired_extension = '.osz'
-	os.makedirs(new_directory, exist_ok=True)
-	print('Downloading beatmaps, please wait.')
+    # Ищем файл "output" в текущей директории
+    output_files = [f for f in os.listdir(current_directory) if f.startswith("output") and f.endswith(".txt")]
+    if not output_files:
+        print("Error: The 'output' file not found in the current directory.")
+    else:
+        # Берем первый найденный файл "output" (можно настроить для нескольких файлов)
+        output_filename = os.path.join(current_directory, output_files[0])
+        new_directory = os.path.join(current_directory, date_str)
+        desired_extension = '.osz'
+        os.makedirs(new_directory, exist_ok=True)
+        print('Downloading beatmaps, please wait.')
 
-	with open (pathChoice, 'r') as file:
-		for line in file:
-			url_to_file = 'https://api.chimu.moe/v1/download/' + line.strip()
-			print(url_to_file)
-			result = subprocess.run(['wget', '--content-disposition', '-P', directory_name, url_to_file])
+        with open(output_filename, 'r') as file:
+            for line in file:
+                url_to_file = 'https://api.chimu.moe/v1/download/' + line.strip()
+                print(url_to_file)
+                response = requests.get(url_to_file)
+                if response.status_code == 200:
+                    file_path = os.path.join(new_directory, line.strip() + desired_extension)
+                    with open(file_path, 'wb') as new_file:
+                        new_file.write(response.content)
+                    print(f"Downloaded: {file_path}")
+                else:
+                    print(f"Error: Failed to download {line.strip()}")
 
 else:
-	print('Please write imp or exp')
+    print('Please write "imp" or "exp"')
